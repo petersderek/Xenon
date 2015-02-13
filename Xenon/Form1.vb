d82ce22f41f23e5bd567608cb8da97f0f0757248
petersderek <peters.m.derek@gmail.com>
2015-02-13 08:40:11 -0600

    Latest Update

--- Xenon/Form1.vb
+++ Xenon/Form1.vb
@@ -112,7 +112,4 @@
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-
-    End Sub
 End Class
