034ad116ee59aa1f8213df44216ff58f1beaf990
petersderek <peters.m.derek@gmail.com>
2015-03-24 21:37:58 -0500

    New Shortcuts Added

--- Xenon/Quick Shortcuts.Designer.vb
+++ Xenon/Quick Shortcuts.Designer.vb
@@ -23,6 +23,8 @@ Partial Class Quick_Shortcuts
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,11 +37,32 @@ Partial Class Quick_Shortcuts
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Quickly navigate through the various parts of Xenon with the following commands:"
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 60)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(77, 13)
+        Me.LinkLabel1.TabIndex = 2
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "browser:history"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Xenon - History"
+        '
         'Quick_Shortcuts
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(416, 254)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Name = "Quick_Shortcuts"
@@ -49,4 +72,6 @@ Partial Class Quick_Shortcuts
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
--- Xenon/Quick Shortcuts.vb
+++ Xenon/Quick Shortcuts.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Quick_Shortcuts
 
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+
+    End Sub
 End Class
